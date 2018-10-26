87b493a0deb266ef621bd581dcc834fa180d7998
Joseph Jeganathan <josephjeganathan@gmail.com>
2018-10-26 13:07:35 +1100

    Refactoring on table valued types

---
+++ Dapperer/QueryBuilders/MsSql/TableValueParams/LongList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Dapperer.QueryBuilders.MsSql.TableValueParams
+{
+    public class LongList : SimpleListTableValueParams<long>
+    {
+        public LongList(IEnumerable<long> records) : base(records, "LongList", "Id")
+        {
+        }
+    }
+}
