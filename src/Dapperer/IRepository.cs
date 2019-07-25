de5762da1ac16467bee0a5759ddb87cefd7bcdf2
Aleksandar Beninski <aleksandar.beninski@bedegaming.com>
2019-07-25 16:32:10 +0300

    [Patch] made identity insert optional in IRepository

--- src/Dapperer/IRepository.cs
+++ src/Dapperer/IRepository.cs
@@ -10,8 +10,8 @@ namespace Dapperer
         IList<TEntity> GetByKeys(IEnumerable<TPrimaryKey> primaryKeys);
         IList<TEntity> GetAll();
         Page<TEntity> Page(int skip, int take);
-        TEntity Create(TEntity entity, bool identityInsert);
-        int Create(IEnumerable<TEntity> entities, bool identityInsert);
+        TEntity Create(TEntity entity, bool identityInsert = false);
+        int Create(IEnumerable<TEntity> entities, bool identityInsert = false);
         int Update(TEntity entity);
         int Delete(TPrimaryKey primaryKey);
         int Delete(string filterQuery, object filterParams = null);
@@ -20,8 +20,8 @@ namespace Dapperer
         Task<IList<TEntity>> GetByKeysAsync(IEnumerable<TPrimaryKey> primaryKeys);
         Task<IList<TEntity>> GetAllAsync();
         Task<Page<TEntity>> PageAsync(int skip, int take);
-        Task<TEntity> CreateAsync(TEntity entity, bool identityInsert);
-        Task<int> CreateAsync(IEnumerable<TEntity> entities, bool identityInsert);
+        Task<TEntity> CreateAsync(TEntity entity, bool identityInsert = false);
+        Task<int> CreateAsync(IEnumerable<TEntity> entities, bool identityInsert = false);
         Task<int> UpdateAsync(TEntity entity);
         Task<int> DeleteAsync(TPrimaryKey primaryKey);
         Task<int> DeleteAsync(string filterQuery, object filterParams = null);
